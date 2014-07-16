64ac2fa51a9ef9e073bf1850a199d4728c8e827d
tomr <tomr@taykey.com>
2014-07-16 22:13:28 +0200

    Maxing code more flexible

--- chess/complexmove/movediff.py
+++ chess/complexmove/movediff.py
@@ -3,10 +3,21 @@ import re,sys
 def main(argv):	
 	for line in sys.stdin:
 		piece,from_location,to_location, surround = line.split("\t")
+		fields = line.strip().split("\t")
+		if len(fields)==4:
+			piece, from_location, to_location, surround = fields
+		elif len(fields)==3:
+			piece, from_location, to_location = fields
+		else:
+			continue
 		from_location = from_location.split(",")
 		to_location = to_location.split(",")
-		print "%s\t%s\t%s\t%s" % (piece, int(to_location[1])-int(from_location[1]), 
-			int(to_location[0]) - int(from_location[0]), surround.strip() )
+		if len(fields)==4:
+			print "%s\t%s\t%s\t%s" % (piece, int(to_location[1])-int(from_location[1]), 
+				int(to_location[0]) - int(from_location[0]), surround)
+		else:
+			print "%s\t%s\t%s\t" % (piece, int(to_location[1])-int(from_location[1]), 
+				int(to_location[0]) - int(from_location[0]))
 		
 if __name__ == '__main__':
 	sys.stderr.write("Calculate move diff: %s\n"%sys.argv)
