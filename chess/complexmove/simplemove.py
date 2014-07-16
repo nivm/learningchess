c2616a25b5bcfb70e2028eec2ad78f993f4e5f31
tomr <tomr@taykey.com>
2014-07-16 22:12:53 +0200

    Bug fixing - x-y

--- chess/complexmove/simplemove.py
+++ chess/complexmove/simplemove.py
@@ -1,7 +1,12 @@
 import re,sys
 import chess
 
+import argparse
+
 def fen_to_piece_placement_array(fen):
+	'''
+	return a 8X8 which represntation the board
+	'''
 	piece_placement = fen.split()[0]
 	piece_placement_array = [list(line) for line in piece_placement.split("/")]
 	piece_placement_array1 = []
@@ -17,95 +22,86 @@ def fen_to_piece_placement_array(fen):
 		piece_placement_array1.append(new_line)
 	return piece_placement_array1
 
-def find_from_to(piece, from_piece_placement_array, to_piece_placement_array):
+def find_from_to(piece, from_array, to_array):
+	'''
+	Retruns (x1, y1), (x2,y2) which represnts the movement of the piece
+	from from_array to to_array
+	'''
 	from_location=()
 	to_location=()
-	for i in xrange(8):
-		for j in xrange(8):
-			if to_piece_placement_array[i][j] != from_piece_placement_array[i][j]:
-				if piece == from_piece_placement_array[i][j]:
-					from_location= (i,j)
-				if piece == to_piece_placement_array[i][j]:
-					to_location = (i,j)
+	for i in xrange(len(from_array)):
+		for j in xrange(len(from_array)):
+			if to_array[i][j] != from_array[i][j]:
+				if piece == from_array[i][j]:
+					from_location= (j,i)
+				if piece == to_array[i][j]:
+					to_location = (j,i)
 	return from_location,to_location
 
-def find_surround(location, board_array):
+def find_surround(location, board_array, radius=1):
+	'''
+	Construct list representing the surrounding of radius
+	x - spot is occiupied
+	y - spot is clear
+	z - spot is out of the board
+	o - piece itself
+
+	example - [y,y,z,y, o, z, z, z, z]
+	This is the surround of radius 1 for the right
+	whie rook on the opening position
+	'''
 	x,y = location[0], location[1]
 	surround = []
-	try:
-		surround.append(board_array[x-1][y-1])
-	except:
-		surround.append("z")
-	try:
-		surround.append(board_array[x][y-1])
-	except:
-		surround.append("z")
-	try:
-		surround.append(board_array[x+1][y-1])
-	except:
-		surround.append("z")
-
-	try:
-		surround.append(board_array[x-1][y])
-	except:
-		surround.append("z")
-	surround.append("o")
-	try:
-		surround.append(board_array[x+1][y])
-	except:
-		surround.append("z")
-	
-	try:
-		surround.append(board_array[x-1][y+1])
-	except:
-		surround.append("z")
-	try:
-		surround.append(board_array[x][y+1])
-	except:
-		surround.append("z")
-	try:
-		surround.append(board_array[x+1][y+1])
-	except:
-		surround.append("z")
-
-	surround = [transalte_surround(s) for s in surround]
+	for i in xrange(-radius, radius+1):
+		for j in xrange(-radius, radius+1):
+			try:
+				if j==0 and i == 0:
+					surround.append("o")
+					continue
+				if board_array[y+i][x+j]==1:
+					surround.append("y")
+				else:
+					surround.append("x")
+			except:
+				surround.append("z")
 	return surround
 
-def transalte_surround(s):
-	if s ==1:
-		return "y"
-	if s == "z" or s=="o":
-		return s
-	else:
-		return "x"
+def parseArguments(args):
 
+	argprs = argparse.ArgumentParser(description="Simple move", add_help=True)
 
+	argprs.add_argument('--radius', required=False, type=int, dest='radius', 
+						help='Surround radius', default=1)
+
+	arguments = argprs.parse_args(args)
+	return arguments
+
+def print_board_diff (board1, board2):
+	for i in xrange(0, len(board1)):
+		print board1[i], '\t',board2[i]
 
 def main(argv):
 	'''
 	parse chess output to a simple move file
 	'''
-	#line="P	rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1	rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq d3 0 1"
-	#line ="Q	k1r5/p1q4P/3p4/3P4/5PP1/2pQ2K1/2R5/8 w - - 3 45	k1r4Q/p1q5/3p4/3P4/5PP1/2pQ2K1/2R5/8 b - - 0 45"
-	#line should be formated as
-	#piece from_fen to_fen
-	#fen http://en.wikipedia.org/wiki/Forsyth-Edwards_Notation
+	arguments = parseArguments(argv[1:])
 	
 	for line in sys.stdin:
 		try:
 			piece,from_fen,to_fen = line.split("\t")
 			from_piece_placement_array = fen_to_piece_placement_array(from_fen)
 			to_piece_placement_array = fen_to_piece_placement_array(to_fen)
-			from_location, to_location = find_from_to(piece, from_piece_placement_array, to_piece_placement_array)
+			from_location, to_location = find_from_to(piece, \
+				from_piece_placement_array, to_piece_placement_array)
 
-			surround = find_surround(from_location, from_piece_placement_array)
-			
+			surround = find_surround(from_location, from_piece_placement_array,\
+				arguments.radius)
+
 			print "%s\t%s,%s\t%s,%s\t%s" \
 			% (piece, from_location[0],from_location[1] , \
 				to_location[0], to_location[1], ";".join(surround))
 		except:
 			pass
-			#sys.stderr.write("Error: " + line + "\n")		
 
 if __name__ == '__main__':
 	sys.stderr.write("parse simple moves: %s\n"%sys.argv)
